21ffdbe549212af252d9e8c1af2ec714a9f119a4
andes028@gmail.com <andes028@gmail.com>
2023-09-10 21:35:20 -0500

    Prueba automatizada choucair

---
+++ src/test/java/co/com/choucair/certification/exito/runners/ComprarProductosAleatoriosRunner.java
@@ -0,0 +1,17 @@
+package co.com.choucair.certification.exito.runners;
+
+
+
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.SnippetType;
+import net.serenitybdd.cucumber.CucumberWithSerenity;
+import org.junit.runner.RunWith;
+
+
+@RunWith(CucumberWithSerenity.class)
+@CucumberOptions(features = "src\\test\\resources\\features\\comprar_productos_aletorios.feature",
+        glue = "co.com.choucair.certification.exito.stepsdefinitions",
+        snippets = SnippetType.CAMELCASE)
+public class ComprarProductosAleatoriosRunner {
+}
